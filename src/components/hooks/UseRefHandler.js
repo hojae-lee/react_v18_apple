96f66ce595f7e67fdc79460f5dff7a4f4d76315b
jary-lee <jary.j.lee@polarissharetech.com>
2023-03-24 17:45:58 +0900

    update : useRef 를 사용한 저장소 활용 방법 (state 랑 비교)

--- src/components/hooks/UseRefHandler.js
+++ src/components/hooks/UseRefHandler.js
@@ -1,18 +1,48 @@
 import { useState, useRef } from "react";
 
 function UseHookHandler() {
+  const [render, setRender] = useState(0);
   const [count, setCount] = useState(1);
+  // ref 는 변화하여도 렌더링되지 않음.
+  const countRef = useRef(0);
+  let countVar = 0;
 
-  const handleCountUpdate = () => {
+  const handleReander = () => {
+    setRender(render + 1);
+  };
+
+  const handleStateCountUpdate = () => {
     setCount(count + 1);
+  };
+
+  const handleRefCountUpdate = () => {
+    countRef.current++;
+  };
+
+  const handleVarCountUpdate = () => {
+    countVar++;
+    console.log(countVar);
   };
 
   return (
     <div>
-      <button type="button" onClick={handleCountUpdate}>
-        Update
+      <button type="button" onClick={handleReander}>
+        Render
       </button>
-      <span>count: {count}</span>
+      <button type="button" onClick={handleStateCountUpdate}>
+        State Update
+      </button>
+      <button type="button" onClick={handleRefCountUpdate}>
+        Ref Update
+      </button>
+      <button type="button" onClick={handleVarCountUpdate}>
+        Var Update
+      </button>
+      <div>
+        <p>count: {count}</p>
+        <p>ref: {countRef.current}</p>
+        <p>var: {countVar}</p>
+      </div>
     </div>
   );
 }
